b16d81a9a6e9dcb733837f16816e16a6816564e8
Khawar Zamman Wani <wanikhawar@gmail.com>
2022-11-01 16:14:24 +0530

    Update inp2vtk.py
    Added DMD and reduced the number of modes

--- python/inp2vtk.py
+++ python/inp2vtk.py
@@ -1,7 +1,13 @@
 # Script to convert inp file to vtk file
 
-inp = open('LPEH_coexec-domain.inp', 'r')
-vtk = open('LPEH_coexec-domain.vtk', 'w')
+def status(string):
+    print("-" * 30)
+    print(string)
+    print("-" * 30)
+
+
+inp = open('NLPFEH_GD025_T60-fluid_domain.inp', 'r')
+vtk = open('NLPFEH_GD025_T60-fluid_domain.vtk', 'w')
 
 vtk.write("# vtk DataFile Version 3.0\n")
 vtk.write("Mesh Data Information\n")
@@ -9,6 +15,8 @@ vtk.write("ASCII\n")
 vtk.write("DATASET UNSTRUCTURED_GRID\n\n")
 
 # READ DATA FROM INP FILE
+status("READING INP FILE")
+
 lines = inp.readlines()
 
 # CREATING A LIST OF COORDINATES
@@ -24,23 +32,17 @@ for i in range(len(lines)):
         nodal_flag = False
         nodal_data.remove('*Element, type=FC3D8\n')
 
+status("WRITING COORDINATES TO VTK")
+
 nodes = []
 for node in nodal_data:
     nodes.append(node.replace(' ', '').replace('\n', '').split(','))
-
-# CONVERT NODAL INDEX TO INT AND NODAL DATA TO FLOAT
-for node in nodes:
-    node[0] = int(node[0])
-    node[1] = float(node[1])
-    node[2] = float(node[2])
-    node[3] = float(node[3])
-
 
 num_nodes = len(nodes)
 vtk.write("POINTS {} float\n".format(num_nodes))
 
 for node in nodes:
-    vtk.write("{0:.7f} {1:.7f} {2:.7f}\n".format(node[1], node[2], node[3]))
+    vtk.write("{0} {1} {2}\n".format(node[1], node[2], node[3]))
 
 # CONNECTIVITY
 conn_data = []
@@ -49,14 +51,15 @@ conn_flag = False
 for i in range(len(lines)):
     if conn_flag:
         conn_data.append(lines[i])
-    if lines[i] == '*Element, type=FC3D8\n':
+    if '*Element' in lines[i]:
         conn_flag = True
-    if lines[i] == '*Nset, nset=Set-1, generate\n':
+    if "*Nset" in lines[i]:
         conn_flag = False
-        conn_data.remove('*Nset, nset=Set-1, generate\n')
+
+status("WRITING ELEMENT CONNECTIVITY DATA TO VTK")
 
 elements = []
-for connectivity in conn_data:
+for connectivity in conn_data[:-1]:
     elements.append(connectivity.replace(' ', '').replace('\n', '').split(','))
 
 num_elem = len(elements)
@@ -64,20 +67,64 @@ vtk.write("\nCELLS {0} {1}\n".format(num_elem, num_elem * 9))
 
 # SUBTRACT ONE FROM EACH NODE NUMBER TO START THE NUMBERING FROM ZERO
 for element in elements:
-    vtk.write("8 {0} {1} {2} {3} {4} {5} {6} {7}\n".format(eval(element[1])-1, eval(element[2])-1, eval(element[3])-1, eval(element[4])-1, eval(element[5])-1,
-                                                           eval(element[6])-1, eval(element[7])-1, eval(element[8])-1))
+    vtk.write(
+        "8 {0} {1} {2} {3} {4} {5} {6} {7}\n".format(eval(element[1]) - 1, eval(element[2]) - 1, eval(element[3]) - 1,
+                                                     eval(element[4]) - 1, eval(element[5]) - 1,
+                                                     eval(element[6]) - 1, eval(element[7]) - 1, eval(element[8]) - 1))
+
+status("WRITING CELL TYPE TO VTK")
 
 vtk.write('\n')
 vtk.write("CELL_TYPES {}\n".format(num_elem))
 for i in range(len(elements)):
     vtk.write("12\n")
 
-vtk.write('\nPOINT_DATA {}\n'.format(num_nodes))
-vtk.write('SCALARS scalars float 1\n')
-vtk.write('LOOKUP_TABLE default\n')
+status("READING POD MODAL DATA")
 
-for i in range(num_nodes):
-    vtk.write(str(i+1) + '\n')
+vel_pod_data = open('file2write.txt', 'r')
+vel_pod_raw = vel_pod_data.readlines()
+vel_pod_loc = []
+for vel_pod_row in vel_pod_raw:
+    vel_pod_loc.append(vel_pod_row.replace(' ', '').replace('\n', '').split(','))
+
+status("WRITING POD MODAL DATA TO VTK")
+vtk.write('\nPOINT_DATA {}\n'.format(num_nodes))
+
+for mode in range(10):
+    if mode + 1 < 10:
+        vtk.write('\nSCALARS vel_POD_mode_0{} float 1\n'.format(mode + 1))
+    else:
+        vtk.write('\nSCALARS vel_POD_mode_{} float 1\n'.format(mode + 1))
+
+    vtk.write('LOOKUP_TABLE default\n')
+    for node in vel_pod_loc:
+        vtk.write(node[mode] + '\n')
+
+vel_pod_data.close()
+
+status("READING DMD MODAL DATA")
+
+vel_dmd_data = open('dmd_modes.txt', 'r')
+vel_dmd_raw = vel_dmd_data.readlines()
+vel_dmd_loc = []
+for vel_dmd_row in vel_dmd_raw:
+    vel_dmd_loc.append(vel_dmd_row.replace(' ', '').replace('\n', '').split(','))
+
+status("WRITING DMD MODAL DATA TO VTK")
+
+for mode in range(20):
+    # since dmd modes are in pairs because of complex conjugate nature, we only take one of the two
+    if mode % 2 == 0:
+        if (((mode + 1) + 1)/2) < 10:
+            vtk.write('\nSCALARS vel_DMD_mode_0{} float 1\n'.format(int(((mode + 1) + 1) / 2)))
+        else:
+            vtk.write('\nSCALARS vel_DMD_mode_{} float 1\n'.format(int(((mode + 1) + 1) / 2)))
+
+        vtk.write('LOOKUP_TABLE default\n')
+        for node in vel_dmd_loc:
+            vtk.write(node[mode] + '\n')
+
+vel_dmd_data.close()
 
 vtk.close()
 inp.close()
